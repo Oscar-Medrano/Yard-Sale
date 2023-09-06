0aa5f95b6837df19ae44a6199ecace66c3562c3c
Oscar A. Medrano <97897533+Oscar-Medrano@users.noreply.github.com>
2023-09-06 13:51:12 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,163 +1,277 @@
-const emailMenu = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
-const mobileMenu = document.querySelector('.mobile-menu');
-const burgerIcon = document.querySelector('.menu');
-const carIcon = document.querySelector('.navbar-shoppingcart');
-const asideShoppingCar = document.querySelector('#shoppingCarDetail');
-const asideProductDetail = document.querySelector('.product-Detail');
-const cardsContainer = document.querySelector('.cards-container');
+let emailMenu           = document.querySelector('.navbar-email');
+let desktopMenu         = document.querySelector('.desktop-menu');
+let iconMenuMobile      = document.querySelector('.menu');
+let mobileMenu          = document.querySelector('.mobile-menu');
+let iconShopping        = document.querySelector('.navbar-shopping-cart');
+let asideShopping       = document.querySelector('.product-detail');
+let divcardscontainer   = document.querySelector('.cards-container')
+let productDetailLeft   = document.querySelector('.product-detail-left')
+let productDetailClose  = document.querySelector('.product-detail-close')
+let myordercontentContainer = document.querySelector('.my-order-content')
+let countCarrito        = document.querySelector('.navbar-shopping-cart div')
+let totalCarrito        = document.querySelector('.total')
+let modal               = document.querySelector('.modal')
+let cerrarModal         = document.querySelector('.boton-modal')
+let productImageInfor   = document.querySelector('.product-detail-left > img:nth-child(2)')
+let labelPriceInfo      = document.querySelector('.product-info-left p:nth-child(1)')
+let labelNameInfo       = document.querySelector('.product-info-left p:nth-child(2)')
+let labelInforInfo      = document.querySelector('.product-info-left p:nth-child(3)')
+let buttonInfo          = document.querySelector('.add-to-cart-button')
+let productosEnCarrito  = []
+let productoACarrito    = []
+
+countCarrito.innerText = document.querySelectorAll('.shopping-cart').length
+totalCarrito.innerText = '$0.00'
+
+emailMenu.addEventListener('click', toggleMenu);
+iconMenuMobile.addEventListener('click', toggleMenuMobile);
+iconShopping.addEventListener('click', toggleAsideShopping)
+productDetailClose.addEventListener('click',closeProductDetail)
+buttonInfo.addEventListener('click', butonClick)
+cerrarModal.addEventListener('click', closeModal)
 
 
-emailMenu.addEventListener('click', toggleDesktop);
-burgerIcon.addEventListener('click', toggleBurger)
-carIcon.addEventListener('click', toggleCar)
-
-
-function toggleDesktop() {
-    asideShoppingCar.classList.add("inactive")
-    desktopMenu.classList.toggle("inactive")
+function butonClick(){
+    agregarCarrito(productoACarrito)
+}
+function toggleMenu(){
+    productDetailLeft.classList.add('inactive')
+    asideShopping.classList.add('inactive');
+    desktopMenu.classList.toggle('inactive');
 }
 
-function toggleBurger() {
-    asideShoppingCar.classList.add("inactive")
-    mobileMenu.classList.toggle("inactive")
+function toggleMenuMobile(){
+    productDetailLeft.classList.add('inactive')
+    asideShopping.classList.add('inactive');
+    mobileMenu.classList.toggle('inactive')
+    
 }
 
-function toggleCar() {
-    mobileMenu.classList.add("inactive")
-    desktopMenu.classList.add("inactive")
-    asideShoppingCar.classList.toggle("inactive")
+function toggleAsideShopping(){
+    productDetailLeft.classList.add('inactive')
+    mobileMenu.classList.add('inactive');
+    desktopMenu.classList.add('inactive')
+    asideShopping.classList.toggle('inactive')
 }
 
-const productList = [];
+function openProductDetail(){
+    asideShopping.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
+    productDetailLeft.classList.remove('inactive')
+}
+
+function closeProductDetail(){
+    productoACarrito = []
+    asideShopping.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
+    productDetailLeft.classList.add('inactive')
+}
+
+function verificarProducto(nameProduct){
+    return productosEnCarrito.some(function(articulo){
+        return articulo === nameProduct
+    })
+}
+
+function closeModal(){
+    modal.style.display = 'none'
+}
+
+let productList = []
 productList.push({
-    name: "Bike",
-    price: "120 USD",
-    image: "https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    name:"Bike",
+    price:120,
+    image:"https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    desc: 'Moutain bike in perfect conditions.With both a front suspension fork and a rear suspension shock, full-suspension mountain bikes help absorb the most impact and chatter from the trail. An added benefit is that it insulates the rider from rough terrain, reducing fatigue and creating a smooth and more comfortable ride. ',
 })
-
 productList.push({
     name: "Air Jordans",
-    price: "50 USD",
+    price: 50,
     image: "https://images.pexels.com/photos/9252069/pexels-photo-9252069.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    desc: 'Show everyone your style with this original Air Jordans shoes. Air Jordan is a line of basketball shoes produced by Nike, Inc. Related apparel and accessories are marketed under Jordan Brand.',
 })
 
 productList.push({
     name: "Beetle Car",
-    price: "500 USD",
+    price:500,
     image: "https://images.pexels.com/photos/9940949/pexels-photo-9940949.jpeg?auto=compress&cs=tinysrgb&w=600",
+    desc: 'The Beetle was a masterpiece of innovation and economics—it was compact, but practical, and inexpensive to buy and maintain. The car’s curved exterior not only gave it a groundbreaking look, but was aerodynamic, too. Rear-wheel drive and its small stature gave the Beetle excellent handling, particularly when compared to the American land yachts of its day, and the innovative air-cooled engine required less maintenance and was easy to fix.',
 })
 
 productList.push({
     name: "Sofa",
-    price: "199 USD",
+    price: 199,
     image: "https://images.pexels.com/photos/276566/pexels-photo-276566.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    desc: 'A brow leather confortable sofa with a modern style, It will look perfect in your living room.'
 })
 
 productList.push({
     name: "Classic Guitar",
-    price: "70 USD",
+    price: 70,
     image: "https://images.pexels.com/photos/1995803/pexels-photo-1995803.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    desc: 'Classic guitar with 3 strings of steel and 3 strings of nylon perfect for beginners and experienced guitarists.',
 })
 
 productList.push({
     name: "Motorcycle",
-    price: "400 USD",
+    price: 400,
     image: "https://images.pexels.com/photos/1715191/pexels-photo-1715191.jpeg?auto=compress&cs=tinysrgb&w=600",
+    desc: 'Cruiser Motorcycle.These hefty machines have the advantage of a low center of gravity, which makes them safer and easier to handle. Cruisers also offer an exceptionally smooth and enjoyable riding experience that could make just about anyone fall in love with riding a motorcycle. ',
 })
 
 
 productList.push({
     name: "Laptop",
-    price: "700 USD",
+    price: 700,
     image: "https://images.pexels.com/photos/7974/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=",
+    desc: 'A suitable laptop wiht enough processing power, memory, and storage to perform challenging programming jobs effectively.',
 })
 
 productList.push({
     name: "Camera",
-    price: "90 USD",
+    price: 190,
     image: "https://images.pexels.com/photos/51383/photo-camera-subject-photographer-51383.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    desc: 'Professional camera brand Canon',
 })
 
 productList.push({
     name: "Nintendo Switch",
-    price: "350 USD",
+    price: 350,
     image: "https://images.pexels.com/photos/371924/pexels-photo-371924.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    desc: 'Nintendo Switch Oled with Mario Kart 8 Deluxe.'
 })
 
 productList.push({
     name: "Dinosaur Toy",
-    price: "15 USD",
+    price: 15,
     image: "https://images.pexels.com/photos/3671194/pexels-photo-3671194.jpeg?auto=compress&cs=tinysrgb&w=600",
+    desc: 'Dinosaur toy, brand Mattel.',
 })
 
 productList.push({
     name: "R2D2 Toy",
-    price: "15 USD",
+    price: 15,
     image: "https://images.pexels.com/photos/2085831/pexels-photo-2085831.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    desc: 'R2D2 toy of Star Wars, brand Mattel.',
 })
 
 productList.push({
     name: "2 Blazers with pants and ties",
-    price: "60 USD",
+    price: 130,
     image: "https://images.pexels.com/photos/3755706/pexels-photo-3755706.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    desc: 'A pair of elegant blazers with silk ties, perfect for special ocations.',
 })
 
-
-
-https://images.pexels.com/photos/276566/pexels-photo-276566.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2
-/*
-<div class="product-card">
-    <img src="https://images.pexels.com/photos/2412368/pexels-photo-2412368.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="">
-    <div class="product-info">
-        <div>
-            <p>120,00 $</p>
-            <p>Bike</p>
-        </div>
-        <figure>
-            <img src="./iconos/bt_add_to_cart.svg" alt="">
-        </figure>
-    </div>
-</div>
-*/
-
-    /*Maquetación HTMl desde JS*/
-function renderProducts(arr) {
-    for (product of arr) {
-        /*CREAMOS UN ELEMENTO DIV Y LE AGREGAMOS LA CLASE product-Card*/
-            const productCard = document.createElement('div');
-            productCard.classList.add('product-card');
-
-         /*CREAMOS UN ELEMENTO img Y LE CAMBIAMOS LA SRC POR EL
-        VALOR QUE SE ENCUENTRE EN EL DICCIONARIO BAJO EL KEY image*/
-            const productImg = document.createElement('img');
-            productImg.setAttribute('src', product.image);
-
-        /*CREAMOS UN ELEMENTO DIV Y LE AGREGAMOS LA CLASE product-info*/
-            const productInfo = document.createElement('div');
-            productInfo.classList.add('product-info');
+function renderListProduct(productList){
+    for (const product of productList) {
+        let divproductCard = document.createElement('div')
+        let imgproductcard = document.createElement('img')
+        let divproductinfo = document.createElement('div')
+        let divdivproductinfo = document.createElement('div')
+        let pprice = document.createElement('p')
+        let pname = document.createElement('p')
+        let pInfo = document.createElement('p')
+        let figureproductinfo = document.createElement('figure')
+        let imgproductinfo = document.createElement('img')
+        divproductCard.classList.add('product-card')
+        imgproductcard.setAttribute('src', product.image)
+        divproductinfo.classList.add('product-info')
+        pprice.innerText ='$' + product.price
+        pname.innerText = product.name
+        pInfo.innerText = product.desc
+        imgproductinfo.setAttribute('src', './icons/bt_add_to_cart.svg')
         
-        /*CREAMOS UN ELEMENTO DIV Y LE AGREGAMOS LAS ETIQUETAS P 
-        QUE VAN A CONTENER EL PRECIO Y EL NOMBRE DEL PRODUCTO*/
-            const productInfoDiv = document.createElement('div');
-        
-            const productPrice = document.createElement('p');
-            productPrice.innerText = product.price;
-            const productName = document.createElement('p');
-            productName.innerText = product.name;
-        
-        /*CREAMOS UN ELEMENTO FIGURE Y UN ELEMENTO IMG 
-        (ESTE POR DENTRO TIENE LA IMAGEN DE AGREGAR AL CARRITO DE COMPRAS)*/
-            const productInfoFigure = document.createElement('figure');
-            const productImgCart = document.createElement('img');
-            productImgCart.setAttribute('src', './iconos/bt_add_to_cart.svg');
+        figureproductinfo.appendChild(imgproductinfo)
+        divdivproductinfo.appendChild(pprice)
+        divdivproductinfo.appendChild(pname)
 
-        /*CON .append ASIGNAMOS CADA ELEMENTO HIJO CON SU PADRE*/
-            cardsContainer.append(productCard);
-            productInfoDiv.append(productPrice, productName);
-            productInfoFigure.append(productImgCart);
-            productCard.append(productImg, productInfo);
-            productInfo.append(productInfoDiv, productInfoFigure);
+        divproductinfo.appendChild(divdivproductinfo)
+        divproductinfo.appendChild(figureproductinfo)
+
+        divproductCard.appendChild(imgproductcard)
+        divproductCard.appendChild(divproductinfo)
+
+        divcardscontainer.appendChild(divproductCard)
+
+        imgproductcard.addEventListener('click', function(){
+            mostrarInfoProduct(product.image, product.price, product.name, product.desc)
+            openProductDetail()
+        });
+
+
+        imgproductinfo.addEventListener('click', function(){
+            let productoAgregado = []
+            productoAgregado.push({
+                name: product.name,
+                price: product.price,
+                image: product.image,
+                desc: product.desc
+            })
+            agregarCarrito(productoAgregado)
+        })
     }
 }
-renderProducts(productList);
+
+/* Funcion para agregar productos al carrito */
+function agregarCarrito(producto){
+    if(verificarProducto(producto[0].name)){
+        productoACarrito= []
+        return modal.style.display = 'grid'
+    }
+
+    /* Creacion de elemtentos HTML */
+    let divShoppingCart = document.createElement('div')
+    let figureShoppingCart = document.createElement('figure')
+    let imgfigureShoppingCart = document.createElement('img')
+    let pnameShoppingCart = document.createElement('p')
+    let ppriceShoppingCart = document.createElement('p')
+    let imgcloseShoppingCart = document.createElement('img')
+
+    /* Se agrega contenido a cada uno de los elementos */
+    divShoppingCart.classList.add('shopping-cart')
+    imgfigureShoppingCart.setAttribute('src', producto[0].image)
+    pnameShoppingCart.innerText = producto[0].name
+    ppriceShoppingCart.innerText ='$' + producto[0].price
+    imgcloseShoppingCart.setAttribute('src','./icons/icon_close.png')
+    imgcloseShoppingCart.classList.add('removeList')
+
+    /* Se insertan los documentos */
+    figureShoppingCart.appendChild(imgfigureShoppingCart)
+    divShoppingCart.appendChild(figureShoppingCart)
+    divShoppingCart.appendChild(pnameShoppingCart)
+    divShoppingCart.appendChild(ppriceShoppingCart)
+    divShoppingCart.appendChild(imgcloseShoppingCart)
+    myordercontentContainer.appendChild (divShoppingCart)
+
+    /* Se agregan la cantidad de productos ademas de la suma de precios */
+    countCarrito.innerText = document.querySelectorAll('.shopping-cart').length
+    totalCarrito.innerText = '$' + (Number(totalCarrito.innerText.substring(1)) + Number(producto[0].price))
+    productosEnCarrito.push(producto[0].name)
+    
+    /* Funcion para quitar productos del carrito */
+    imgcloseShoppingCart.addEventListener('click', function(){
+        divShoppingCart.remove()
+        countCarrito.innerText = document.querySelectorAll('.shopping-cart').length
+        totalCarrito.innerText = '$' + (Number(totalCarrito.innerText.substring(1)) - Number(producto[0].price))
+        productosEnCarrito.splice(productosEnCarrito.indexOf(producto[0].name),1)
+    }) 
+
+}
+
+function mostrarInfoProduct(imagen, precio, nombre, descripcion){
+    productImageInfor.setAttribute('src', imagen)
+    labelPriceInfo.innerText = '$' + precio
+    labelNameInfo.innerText = nombre
+    labelInforInfo.innerText = descripcion
+    productoACarrito = []
+    productoACarrito.push({
+        name: nombre,
+        price: precio,
+        image: imagen,
+        desc: descripcion
+    })
+}
+
+renderListProduct(productList);
